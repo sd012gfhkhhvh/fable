495b4de02df4abdc3962eb739579a664308a261c
SOHAM DAS <sohamdas.nest@gmail.com>
2024-03-31 13:20:32 +0530

    created connection pool and db schema

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,24 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-const app = new Hono()
+// Although DATABASE_URL(secret) is not mentioned in the wrangler.toml file as a variable 
+// but it is stored inside the .dev.vars file which is added to the worker(npx wrangler secret put DATABASE_URL)
+// ref(Secrets) - https://developers.cloudflare.com/workers/configuration/secrets/#secrets-in-development
+type Bindings = {
+  DATABASE_URL: string;
+};
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const app = new Hono<{ Bindings: Bindings }>();
 
-export default app
+app.get("/", (c) => {
+  console.log(c.env.DATABASE_URL);
+
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  return c.text("Hello Hono!");
+});
+
+export default app;
